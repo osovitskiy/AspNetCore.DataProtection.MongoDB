ae8ad4ed932b8146dbd1ae3999a3d45c252e3580
osovitskiy <osovitskiy@yandex.ru>
2017-10-10 20:04:31 +0300

    basic implementation

---
+++ src/ProjectUnknown.AspNetCore.DataProtection.MongoDB/MongoDataProtectionBuilderExtensions.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ProjectUnknown.AspNetCore.DataProtection.MongoDB
+{
+    public static class MongoDataProtectionBuilderExtensions
+    {
+        private const string DataProtectionKeysName = "DataProtection-Keys";
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified collection in MongoDB database.
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="collectionFactory">The factory used to create <see cref="IMongoCollection{BsonDocument}"/> instances.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder"/> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToMongo(this IDataProtectionBuilder builder, Func<IMongoCollection<BsonDocument>> collectionFactory)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (collectionFactory == null)
+            {
+                throw new ArgumentNullException(nameof(collectionFactory));
+            }
+
+            return PersistKeysToMongoInternal(builder, collectionFactory);
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified collection in MongoDB database.
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="client">The <see cref="IMongoClient"/> for database access.</param>
+        /// <param name="database">The name of the database.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder"/> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToMongo(this IDataProtectionBuilder builder, IMongoClient client, string database)
+        {
+            return PersistKeysToMongo(builder, client, database, DataProtectionKeysName);
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified collection in MongoDB database.
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="client">The <see cref="IMongoClient"/> for database access.</param>
+        /// <param name="database">The name of the database.</param>
+        /// <param name="collection">The name of the collection.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder"/> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToMongo(this IDataProtectionBuilder builder, IMongoClient client, string database, string collection)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (string.IsNullOrEmpty(database))
+            {
+                throw new ArgumentNullException(nameof(database));
+            }
+
+            if (string.IsNullOrEmpty(collection))
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            return PersistKeysToMongoInternal(builder, () => client.GetDatabase(database).GetCollection<BsonDocument>(collection));
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified collection in MongoDB database.
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="connectionString">Connection string to MongoDB server.</param>
+        /// <param name="database">The name of the database.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder"/> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToMongo(this IDataProtectionBuilder builder, string connectionString, string database)
+        {
+            return PersistKeysToMongo(builder, connectionString, database, DataProtectionKeysName);
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified collection in MongoDB database.
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="connectionString">Connection string to MongoDB server.</param>
+        /// <param name="database">The name of the database.</param>
+        /// <param name="collection">The name of the collection.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder"/> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToMongo(this IDataProtectionBuilder builder, string connectionString, string database, string collection)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            if (string.IsNullOrEmpty(database))
+            {
+                throw new ArgumentNullException(nameof(database));
+            }
+
+            if (string.IsNullOrEmpty(collection))
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            var client = new MongoClient(connectionString);
+
+            return PersistKeysToMongoInternal(builder, () => client.GetDatabase(database).GetCollection<BsonDocument>(collection));
+        }
+
+        private static IDataProtectionBuilder PersistKeysToMongoInternal(IDataProtectionBuilder builder, Func<IMongoCollection<BsonDocument>> collectionFactory)
+        {
+            builder.Services.TryAddSingleton<IXmlRepository>(services => new MongoXmlRepository(collectionFactory));
+
+            return builder;
+        }
+    }
+}
---
+++ src/ProjectUnknown.AspNetCore.DataProtection.MongoDB/MongoXmlRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ProjectUnknown.AspNetCore.DataProtection.MongoDB
+{
+    /// <summary>
+    /// An <see cref="IXmlRepository"/> which is backed by MongoDB.
+    /// </summary>
+    public class MongoXmlRepository : IXmlRepository
+    {
+        private readonly Func<IMongoCollection<BsonDocument>> collectionFactory;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MongoXmlRepository"/>.
+        /// </summary>
+        /// <param name="collectionFactory">The factory used to create <see cref="IMongoCollection{BsonDocument}"/> instances.</param>
+        public MongoXmlRepository(Func<IMongoCollection<BsonDocument>> collectionFactory)
+        {
+            this.collectionFactory = collectionFactory;
+        }
+
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            return GetAllElementsCore().ToList().AsReadOnly();
+        }
+
+        private IEnumerable<XElement> GetAllElementsCore()
+        {
+            var collection = collectionFactory();
+            var cursor = collection.FindSync(FilterDefinition<BsonDocument>.Empty);
+
+            while (cursor.MoveNext())
+            {
+                foreach (var item in cursor.Current)
+                {
+                    if (item.TryGetElement("xml", out var value) && value.Value.BsonType == BsonType.String)
+                    {
+                        yield return XElement.Parse(value.Value.AsString);
+                    }
+                    else
+                    {
+                        throw new Exception("Invalid document.");
+                    }
+                }
+            }
+        }
+
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            var collection = collectionFactory();
+            var document = new BsonDocument
+            {
+                new BsonElement("xml", element.ToString(SaveOptions.DisableFormatting))
+            };
+
+            collection.InsertOne(document);
+        }
+    }
+}
